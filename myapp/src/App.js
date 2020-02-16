d3c79f080553699ca7ca919f5f57ba44ee65e9ae
田端佑也 <tabayu5@gmail.com>
2020-02-16 14:59:07 +0900

    add_material-ui

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Divider from '@material-ui/core/Divider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [
+        {id:"1", text: "todo 1"},
+        {id:"2", text: "todo 2"},
+        {id:"3", text: "todo 3"}
+      ]
+    };
+  }
+
+  deleteTodo(id) {
+    let todos = this.state.todos;
+    todos = todos.filter((todo) => todo.id !== id );
+    this.setState({todos: todos});
+  }
+
+  render() {
+    return (
+      <List>
+        {
+          this.state.todos.map((todo) => {
+            return (
+              <div>
+                <ListItem key={todo.id}>
+                  <ListItemText>{todo.text}</ListItemText>
+                  <IconButton variant='fab' aria-label='delete' onClick={this.deleteTodo.bind(this, todo.id)}><DeleteIcon /></IconButton>
+                </ListItem>
+                <Divider />
+              </div>
+            )
+          })
+        }
+      </List>
+    );
+  }
 }
+
 
 export default App;
